543ff09ba42de29f6a29acc1d0686eaf6ce3b072
Jeewoo Kim <jeewookim4@gmail.com>
2015-09-21 14:46:16 -0400

    layout 2 was saved without an xml the first time

--- app/src/main/java/com/example/a2016jkim/helloworld/MainActivity.java
+++ app/src/main/java/com/example/a2016jkim/helloworld/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.a2016jkim.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "TextView Pressed", Toast.LENGTH_SHORT).show();
+                Log.i("onCreate", "TextViewPressed");
+            }
+        });
     }
 
     @Override
