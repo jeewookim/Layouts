b97c605598bc07de8f23518eb90219bdea5994d4
Jeewoo Kim <jeewookim4@gmail.com>
2015-09-23 10:41:22 -0400

    the layout assignment is done

---
+++ app/src/main/java/com/example/a2016jkim/helloworld/AppCompatActivity.java
@@ -0,0 +1,11 @@
+package com.example.a2016jkim.helloworld;
+
+import android.os.Bundle;
+
+/**
+ * Created by 2016jkim on 9/21/2015.
+ */
+public class AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+    }
+}
